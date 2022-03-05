75b645fe1d0a577f40c8a1325f93a23f102f3f52
zehpavora <zehpavora@gmail.com>
2022-03-05 16:44:08 -0300

    Creating random generator for noise samples

---
+++ NesApu/Utilities/IRandomGenerator.cs
@@ -0,0 +1,13 @@
+﻿namespace NesApu.Utilities;
+
+/// <summary>
+/// Generates a random number
+/// </summary>
+public interface IRandomGenerator
+{
+    /// <summary>
+    /// Generates a random <see cref="double"/> value
+    /// </summary>
+    /// <returns><see cref="double"/> value</returns>
+    double NextDouble();
+}
---
+++ NesApu/Utilities/RandomGenerator.cs
@@ -0,0 +1,19 @@
+﻿namespace NesApu.Utilities;
+
+/// <summary>
+/// Implements <see cref="IRandomGenerator"/> using the plain old <see cref="System.Random"/>
+/// </summary>
+public sealed record RandomGenerator : IRandomGenerator
+{
+    #region Constants
+    private static Random Random { get; } = new();
+    #endregion
+
+    /// <inheritdoc/>
+    public double NextDouble()
+    {
+#pragma warning disable SCS0005 // Not for crypto purposes
+        return Random.NextDouble();
+#pragma warning restore SCS0005 // Not for crypto purposes
+    }
+}
