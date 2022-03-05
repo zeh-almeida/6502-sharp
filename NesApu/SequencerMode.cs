34203214604dd87cb9aaf7ee216ba36190eb7da3
zehpavora <zehpavora@gmail.com>
2022-03-05 17:04:58 -0300

    Addming missing docs for sequencer mode

--- NesApu/SequencerMode.cs
+++ NesApu/SequencerMode.cs
@@ -1,8 +1,18 @@
 ﻿namespace NesApu
 {
+    /// <summary>
+    /// Types of sequencers for the APU
+    /// </summary>
     public enum SequencerMode
     {
+        /// <summary>
+        /// Executes the sequencer in four-step mode
+        /// </summary>
         FourStep,
+
+        /// <summary>
+        /// Executes the sequencer in five-step mode
+        /// </summary>
         FiveStep,
     }
 }
