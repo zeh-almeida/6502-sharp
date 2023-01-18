ed6449644cf3037db0d7fef3cfe4d8224e18cb9a
José Ricardo Prado de Almeida <zehpavora@gmail.com>
2023-01-18 11:04:35 -0300

    Initial definition of view models

---
+++ Cpu.MVVM/FlagModel.cs
@@ -0,0 +1,92 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Cpu.Flags;
+
+namespace Cpu.MVVM;
+
+/// <summary>
+/// View Model representation of a <see cref="IFlagManager"/>
+/// </summary>
+public sealed class FlagModel : ObservableObject
+{
+    #region Attributes
+    private bool _isCarry;
+
+    private bool _isZero;
+
+    private bool _isInterruptDisable;
+
+    private bool _isDecimalMode;
+
+    private bool _isBreakCommand;
+
+    private bool _isOverflow;
+
+    private bool _isNegative;
+    #endregion
+
+    #region Properties
+    /// <inheritdoc cref="IFlagManager.IsCarry"/>
+    public bool IsCarry
+    {
+        get => this._isCarry;
+        set => this.SetProperty(ref this._isCarry, value);
+    }
+
+    /// <inheritdoc cref="IFlagManager.IsZero"/>
+    public bool IsZero
+    {
+        get => this._isZero;
+        set => this.SetProperty(ref this._isZero, value);
+    }
+
+    /// <inheritdoc cref="IFlagManager.IsInterruptDisable"/>
+    public bool IsInterruptDisable
+    {
+        get => this._isInterruptDisable;
+        set => this.SetProperty(ref this._isInterruptDisable, value);
+    }
+
+    /// <inheritdoc cref="IFlagManager.IsDecimalMode"/>
+    public bool IsDecimalMode
+    {
+        get => this._isDecimalMode;
+        set => this.SetProperty(ref this._isDecimalMode, value);
+    }
+
+    /// <inheritdoc cref="IFlagManager.IsBreakCommand"/>
+    public bool IsBreakCommand
+    {
+        get => this._isBreakCommand;
+        set => this.SetProperty(ref this._isBreakCommand, value);
+    }
+
+    /// <inheritdoc cref="IFlagManager.IsOverflow"/>
+    public bool IsOverflow
+    {
+        get => this._isOverflow;
+        set => this.SetProperty(ref this._isOverflow, value);
+    }
+
+    /// <inheritdoc cref="IFlagManager.IsNegative"/>
+    public bool IsNegative
+    {
+        get => this._isNegative;
+        set => this.SetProperty(ref this._isNegative, value);
+    }
+    #endregion
+
+    /// <summary>
+    /// Updates the model based on the source
+    /// </summary>
+    /// <param name="source"><see cref="IFlagManager"/> with the values to update from</param>
+    public void Update(IFlagManager source)
+    {
+        this.IsZero = source.IsZero;
+        this.IsCarry = source.IsCarry;
+        this.IsOverflow = source.IsOverflow;
+        this.IsNegative = source.IsNegative;
+        this.IsDecimalMode = source.IsDecimalMode;
+        this.IsBreakCommand = source.IsBreakCommand;
+        this.IsInterruptDisable = source.IsInterruptDisable;
+    }
+}
---
+++ Cpu.MVVM/RegisterModel.cs
@@ -0,0 +1,72 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Cpu.Registers;
+
+namespace Cpu.MVVM;
+
+/// <summary>
+/// View Model representation of a <see cref="IRegisterManager"/>
+/// </summary>
+public sealed class RegisterModel : ObservableObject
+{
+    #region Attributes
+    private ushort _programCounter;
+
+    private byte _stackPointer;
+
+    private byte _accumulator;
+
+    private byte _indexX;
+
+    private byte _indexY;
+    #endregion
+
+    #region Properties
+    /// <inheritdoc cref="IRegisterManager.ProgramCounter"/>
+    public ushort ProgramCounter
+    {
+        get => this._programCounter;
+        set => this.SetProperty(ref this._programCounter, value);
+    }
+
+    /// <inheritdoc cref="IRegisterManager.StackPointer"/>
+    public byte StackPointer
+    {
+        get => this._stackPointer;
+        set => this.SetProperty(ref this._stackPointer, value);
+    }
+
+    /// <inheritdoc cref="IRegisterManager.Accumulator"/>
+    public byte Accumulator
+    {
+        get => this._accumulator;
+        set => this.SetProperty(ref this._accumulator, value);
+    }
+
+    /// <inheritdoc cref="IRegisterManager.IndexX"/>
+    public byte IndexX
+    {
+        get => this._indexX;
+        set => this.SetProperty(ref this._indexX, value);
+    }
+
+    /// <inheritdoc cref="IRegisterManager.IndexY"/>
+    public byte IndexY
+    {
+        get => this._indexY;
+        set => this.SetProperty(ref this._indexY, value);
+    }
+    #endregion
+
+    /// <summary>
+    /// Updates the model based on the source
+    /// </summary>
+    /// <param name="source"><see cref="IRegisterManager"/> with the values to update from</param>
+    public void Update(IRegisterManager source)
+    {
+        this.IndexX = source.IndexX;
+        this.IndexY = source.IndexY;
+        this.Accumulator = source.Accumulator;
+        this.StackPointer = source.StackPointer;
+        this.ProgramCounter = source.ProgramCounter;
+    }
+}
