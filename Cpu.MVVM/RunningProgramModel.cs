4762c8a11552a795666c0d9e0fc301d80a129ea0
José Ricardo Prado de Almeida <zehpavora@gmail.com>
2023-01-20 10:49:50 -0300

    Adding model to track instruction execution

---
+++ Cpu.MVVM/RunningProgramModel.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Cpu.Execution;
+using System.Collections.ObjectModel;
+
+namespace Cpu.MVVM;
+
+/// <summary>
+/// Represents the executed instructions in a program
+/// </summary>
+public partial class RunningProgramModel : ObservableObject
+{
+    #region Properties
+    /// <summary>
+    /// Instructions previously executed
+    /// </summary>
+    private ObservableCollection<DecodedInstruction> Instructions { get; }
+    #endregion
+
+    #region Constructors
+    /// <summary>
+    /// Instantiates a new view model
+    /// </summary>
+    public RunningProgramModel()
+    {
+        this.Instructions = new ObservableCollection<DecodedInstruction>();
+    }
+    #endregion
+
+    /// <summary>
+    /// Adds a new executed instruction to the running program
+    /// </summary>
+    /// <param name="instruction">Instruction to add</param>
+    [RelayCommand]
+    public void AddInstruction(DecodedInstruction instruction)
+    {
+        this.Instructions.Add(instruction);
+    }
+
+    /// <summary>
+    /// Clears the execution list
+    /// </summary>
+    [RelayCommand]
+    public void ClearExecution()
+    {
+        this.Instructions.Clear();
+    }
+}
