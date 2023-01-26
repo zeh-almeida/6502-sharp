7f95307f539b5fd680368a0940a72e22d2d4f9d2
José Ricardo Prado de Almeida <zehpavora@gmail.com>
2023-01-26 10:08:37 -0300

    Adding dependency injection for CPU View Models

--- Cpu.Maui/MauiProgram.cs
+++ Cpu.Maui/MauiProgram.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Markup;
+using Cpu.Mvvm.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Cpu.Maui;
 public static class MauiProgram
@@ -6,13 +9,17 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder
-            .UseMauiApp<App>()
+
+        builder.UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .UseMauiCommunityToolkitMarkup()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
+
+        builder.Services.Add6502CpuMvvm();
 
 #if DEBUG
         builder.Logging.AddDebug();
